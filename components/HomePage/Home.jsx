f1524a7dc5d4d8ada1e94dc9303b7f70b1ec4328
Onurhan Yuvaktaş <onurhanyuvaktas@outlook.com>
2025-05-21 19:02:54 +0300

    Complete
    complete the project

---
+++ components/HomePage/Home.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./home.css";
+export default function Home() {
+    return (
+        <div className="home-container">
+        <div className="home">
+          <img className="home-logo" src="images/iteration-1-images/logo.svg" />
+          <p className="home-text">
+            KOD ACIKTIRIR <br /> PİZZA, DOYURUR{" "}
+          </p>
+  
+          <Link className="btn" to="/order">
+            ACIKTIM
+          </Link>
+        </div>
+      </div>
+    );
+  }
